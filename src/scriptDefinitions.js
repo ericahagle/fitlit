2b46bfce36c06562ee30ba738821747824aa78d4
Erica Hagle <ericahagle.dev@gmail.com>
2023-10-16 15:46:16 -0700

    test: Add unit tests for the selectCurrentUser function

--- src/scriptDefinitions.js
+++ src/scriptDefinitions.js
@@ -2,4 +2,28 @@ function generateRandomUserID() {
   return Math.floor(Math.random() * 10) + 1;
 }
 
-module.exports = { generateRandomUserID };
+function selectCurrentUser(userId, users, hydrationData, sleep, activity) {
+  const user = users.find((u) => {
+    return u.id === userId;
+  });
+
+  if (!user) {
+    return 'User not found!';
+  }
+
+  const userHydrationData = hydrationData.filter((hData) => {
+    return hData.userID === userId;
+  });
+
+  // added activity and sleep keys data will be added later
+  const currentUser = {
+    ...user,
+    hydrationData: userHydrationData || [],
+    activity: activity || [],
+    sleep: sleep || []
+  };
+
+  return currentUser;
+}
+
+module.exports = { generateRandomUserID, selectCurrentUser };
