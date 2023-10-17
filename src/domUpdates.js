86184f4fa8e117cbbb9789cdc81e52fe7a2870ed
Brendan Turner <kisbyturner@gmail.com>
2023-10-17 09:50:04 -0600

    Remove tests

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,20 +1,10 @@
 //NOTE: Your DOM manipulation will occur in this file
-const userName = document.queryselector('#welcomeMat');
-//Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
-const exampleFunction1 = (person) => {
-  console.log(`oh hi there ${person}`)
-}
-
-const exampleFunction2 = (person) => {
-  console.log(`bye now ${person}`)
-}
+const userName = document.querySelector('#welcomeMat');
 
 const updateUserName = (userData) => {
-  userName.innerHTML = `<h1>${userData.name}</h1>`;
+  userName.innerHTML += `<h1>${userData.name}</h1>`;
 }
 
 export {
-  exampleFunction1,
-  exampleFunction2,
   updateUserName
 }
