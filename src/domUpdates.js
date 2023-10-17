764f875b0cb2b36cacc500431d9b6b2d439fa92b
Brendan Turner <kisbyturner@gmail.com>
2023-10-17 02:23:13 -0600

    add the updateUserName function

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,5 @@
 //NOTE: Your DOM manipulation will occur in this file
-
+const userName = document.queryselector('#welcomeMat');
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
 const exampleFunction1 = (person) => {
   console.log(`oh hi there ${person}`)
@@ -9,11 +9,12 @@ const exampleFunction2 = (person) => {
   console.log(`bye now ${person}`)
 }
 
+const updateUserName = (userData) => {
+  userName.innerHTML = `<h1>${userData.name}</h1>`;
+}
 
 export {
   exampleFunction1,
   exampleFunction2,
+  updateUserName
 }
-
-
-  
