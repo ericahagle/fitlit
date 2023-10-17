71b659162b8078538c58b70f69780c701e91597c
Brendan Turner <kisbyturner@gmail.com>
2023-10-17 14:27:57 -0600

    Changes to Arrow Functions

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -3,12 +3,12 @@ const waterDay = document.querySelector('#waterDay');
 const waterWeek = document.querySelector('#waterWeek')
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
 
-function waterDayUpdate(date, hydrationData) {
+const  waterDayUpdate = (date, hydrationData) => {
   waterDay.innerHTML = '';
   waterDay.innerHTML = `<div> <h1>${date}</h1> <p>${hydrationData}</p>`
 }
 
-function waterWeekUpdate(date, hydrationArray) {
+const  waterWeekUpdate = (date, hydrationArray) => {
   waterWeek.innerHTML = '';
   hydrationArray.forEach(element => waterWeek.innerHTML += `<div><p>${element.date}</p><p>${element.mumOunces}</p></div>` )
 }
