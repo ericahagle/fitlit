602eabbd176140e9e46dab67bc8f3a03042ef2f8
Brendan Turner <kisbyturner@gmail.com>
2023-10-17 02:12:57 -0600

    Remove event listener

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,4 @@
 //NOTE: Your DOM manipulation will occur in this file
-const body = document.querySelector('body');
 const userInfo = document.querySelector('#userData');
 
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
@@ -11,15 +10,14 @@ const exampleFunction2 = (person) => {
   console.log(`bye now ${person}`)
 }
 
-body.addEventListener("load", displayUserInfo())
+const displayUserInfo = (user) => {
+  userInfo.innerHTML = '';
+  userInfo.innerHTML = `<p>${user.address}</p> <p>${user.email}</p>`;
+}
 export {
   exampleFunction1,
   exampleFunction2,
   displayUserInfo
 }
 
-const displayUserInfo = (user) => {
-  userInfo.innerHTML = '';
-  userInfo.innerHTML = `<p>${user.address}</p> <p>${user.email}</p>`;
-}
   
