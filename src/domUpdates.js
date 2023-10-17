5baa497480195e713b565130eca8c32098714f0b
Brendan Turner <kisbyturner@gmail.com>
2023-10-17 14:15:29 -0600

    Hydration Dom Updates

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,18 +1,21 @@
 //NOTE: Your DOM manipulation will occur in this file
-
+const waterDay = document.querySelector('#waterDay');
+const waterWeek = document.querySelector('#waterWeek')
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
-const exampleFunction1 = (person) => {
-  console.log(`oh hi there ${person}`)
+
+function waterDayUpdate(date, hydrationData) {
+  waterDay.innerHTML = '';
+  waterDay.innerHTML = `<div> <h1>${date}</h1> <p>${hydrationData}</p>`
 }
 
-const exampleFunction2 = (person) => {
-  console.log(`bye now ${person}`)
+function waterWeekUpdate(date, hydrationArray) {
+  waterWeek.innerHTML = '';
+  hydrationArray.forEach(element => waterWeek.innerHTML += `<div><p>${element.date}</p><p>${element.mumOunces}</p></div>` )
 }
-
 
 export {
-  exampleFunction1,
-  exampleFunction2,
+  waterDayUpdate,
+  waterWeekUpdate
 }
 
 
