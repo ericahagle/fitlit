8bbb89c38623679fe6cccbf5b0b7efda367dbfab
Erica Hagle <ericahagle.dev@gmail.com>
2023-10-18 22:10:48 -0700

    refactor: Update generateRandomUserID function to utilize array of all users

--- src/scriptDefinitions.js
+++ src/scriptDefinitions.js
@@ -1,6 +1,8 @@
 ////////////////////* Generate random number *////////////////////
-function generateRandomUserID() {
-  return Math.floor(Math.random() * 10) + 1;
+function generateRandomUserID(usersArray) {
+	let randomUserId = Math.floor(Math.random() * usersArray.length) + 1;
+	console.log(randomUserId);
+	return randomUserId;
 }
 
 ////////////////////* Create userObject card *////////////////////
