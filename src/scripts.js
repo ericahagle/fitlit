8a7d06b68cdf53d1ed00bbd22b3f7ef421f4e64c
Brendan Turner <kisbyturner@gmail.com>
2023-10-20 17:22:33 -0600

    Fix merge issues

--- src/scripts.js
+++ src/scripts.js
@@ -5,13 +5,16 @@ let hydrationData = null;
 let activityData = null;
 let sleepData = null;
 
+/////////// Import CSS File /////////////
+import './css/styles.css';
 
-// An example of how you tell webpack to use a CSS file
-  import './css/styles.css';
+//////////// Import fetch call from apiCalls.js //////////////
+import { fetchAllTheData } from './apiCalls';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-  import './images/turing-logo.png';
+//////////// Import functions from scriptDefinitions //////////////
+import { generateRandomUserID } from './scriptDefinitions';
 
+///////////// Import functions from domUpdates.js ///////////////
 import {  updateUserName,
     displayUserInfo,
     waterDayUpdate,
@@ -22,4 +25,21 @@ import {  updateUserName,
     stepsDayUpdate,
     activeMinutesUpdate,
     stepsWeekUpdate} from './domUpdates';
+
+    ////////// Event Listeners //////////
+window.addEventListener('load', () => {
+  fetchAllTheData()
+	.then(data => {
+		allUsers = allUsers;
+		// console.log(allUsers);
+		currentUser = allUsers[generateRandomUserID(allUsers) - 1];
+		// console.log(currentUser);
+		hydrationData = hydrationData;
+		// console.log(hydrationData);
+		activityData = activityData;
+		// console.log(activityData);
+		sleepData = sleepData;
+		// console.log(sleepData);
+	});
+});
 
