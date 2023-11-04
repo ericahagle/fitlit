a9af99e5af6abb7190365eff03d5402311b62642
Brendan Turner <kisbyturner@gmail.com>
2023-11-04 16:14:43 -0600

    Remove toggleAdmin function from load

--- src/scripts.js
+++ src/scripts.js
@@ -75,7 +75,6 @@ window.addEventListener('load', () => {
     stepsWeekUpdate(checkStepGoal7Days(completeCurrentUser));
     stepsGoalCompare(findStepGoalAverage(allUsers));
     sleepLifeUpdate(calculateAverageSleepQuality(completeCurrentUser), calculateAverageHoursSlept(completeCurrentUser));
-    toggleAdmin()
   });
 });
     
